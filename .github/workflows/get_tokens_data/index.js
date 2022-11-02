dab3f0f4c69e081f756fb3f11f6cec5281f31d2b
pabloromeroelux <pablo.romero-jaren@electrolux.com>
2022-11-02 22:31:01 +0100

    trying stuff

--- .github/workflows/get_tokens_data/index.js
+++ .github/workflows/get_tokens_data/index.js
@@ -1,7 +1,10 @@
 // https://docs.github.com/en/actions/creating-actions/creating-a-javascript-action
 
 var core = require("@actions/core");
-var axios = require("axios");
-axios.get("../../../token_values/reference.json").then(function (dara) {
-  core.setOutput("data", JSON.stringify(data));
-});
+var config = require("fs").readFileSync(
+  "../../../token_values/reference.json",
+  "utf-8"
+);
+var obj = JSON.parse(config);
+
+core.setOutput("data", JSON.stringify(obj));
