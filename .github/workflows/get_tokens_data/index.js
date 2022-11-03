73194402e7d701be1071d37edec0dc2aeb0622c4
pabloromeroelux <pablo.romero-jaren@electrolux.com>
2022-11-03 12:44:53 +0100

    update structure for 3 sources

--- .github/workflows/get_tokens_data/index.js
+++ .github/workflows/get_tokens_data/index.js
@@ -1,7 +1,10 @@
 // https://docs.github.com/en/actions/creating-actions/creating-a-javascript-action
 
 var core = require("@actions/core");
-var config = require("fs").readFileSync("token_values/reference.json", "utf-8");
+var config = require("fs").readFileSync(
+  "token_values/web/reference.json",
+  "utf-8"
+);
 var obj = JSON.parse(config);
 var data = { reference: obj };
 core.setOutput("data", JSON.stringify(data));
